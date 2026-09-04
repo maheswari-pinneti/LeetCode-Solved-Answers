5af73f03ca06b8c1e9b04b381516dfccde007a4c
Maheswari Pinneti <pinnetimaheswari17@gmail.com>
2026-09-04 21:23:54 +0530

    Time: 1 ms (62.93%), Space: 59.2 MB (71.88%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.ts
@@ -0,0 +1,11 @@
+function getConcatenation(nums: number[]): number[] {
+    const n = nums.length;
+    const ans = new Array<number>(2 * n);
+
+    for (let i = 0; i < n; i++) {
+        ans[i] = nums[i];
+        ans[i + n] = nums[i];
+    }
+
+    return ans;
+}
