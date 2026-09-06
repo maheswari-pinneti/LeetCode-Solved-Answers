6ded88c870b9c292cd9c85d6dc8c49b93265d822
Maheswari Pinneti <pinnetimaheswari17@gmail.com>
2026-09-06 21:31:41 +0530

    Time: 2 ms (44.65%), Space: 55.7 MB (70.39%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.ts
@@ -0,0 +1,16 @@
+function generateParenthesis(n: number): string[] {
+    const result: string[] = [];
+
+    function backtrack(s: string, open: number, close: number) {
+        if (s.length === 2 * n) {
+            result.push(s);
+            return;
+        }
+
+        if (open < n) backtrack(s + "(", open + 1, close);
+        if (close < open) backtrack(s + ")", open, close + 1);
+    }
+
+    backtrack("", 0, 0);
+    return result;
+}
